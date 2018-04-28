d832a9e71e2775688ad13e307dcc6c53a0d8dd1d
jacyhowe <jacyhowe@hotmail.com>
2018-04-27 19:01:01 -0600

    added remove course reducer and dispatched the remove course action

--- src/components/schedule.js
+++ src/components/schedule.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
+import { removeCourse } from '../actions';
 
 class Schedule extends Component {
 
@@ -11,9 +12,9 @@ class Schedule extends Component {
  
     renderCourse(course) {
         return ( 
-            <div key={this.props.courses.indexOf(course)} className="slot">
-                <div className="slot-title">{course.title}</div>
-                <a className="action-slot-remove">remove course</a>
+            <div key={this.props.courses.indexOf(course)} className={`slot ${course.enrolled ? 'slot-course' : 'slot-empty'}`}>
+                <div className="slot-title">{course.enrolled ? course.title : 'Empty Slot'}</div>
+                <a className={`action-slot-remove ${course.enrolled ? 'show-content' : 'hide-content'}`} onClick={() => this.props.removeCourse(course)}>remove course</a>
             </div>        
                     
         )
@@ -36,4 +37,12 @@ function mapStateToProps(state) {
     return { courses: state.courses };
 }
 
-export default connect(mapStateToProps)(Schedule);
+function mapDispatchToProps(dispatch) {
+    return {
+        removeCourse: (course) => {
+            dispatch(removeCourse(course))
+    }
+}  
+}
+
+export default connect(mapStateToProps, mapDispatchToProps)(Schedule);
