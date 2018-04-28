4a46c9cbd8ce66171257842cd33569807ab39e78
jacyhowe <jacyhowe@hotmail.com>
2018-04-27 18:31:10 -0600

    implemented remove course action

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,4 +1,7 @@
-import { FETCH_COURSES } from './types';
+import { FETCH_COURSES, 
+        REMOVE_COURSE } 
+        from './types';
+
 
 export function fetchCourses() {
     return {
@@ -14,4 +17,13 @@ export function fetchCourses() {
             }
         ]
     }
+}
+
+
+export function removeCourse(course) {
+    return {
+        type: REMOVE_COURSE,
+        payload: course
+    }
+
 }
